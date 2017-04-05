fbec0309bc4fecd5f567a57f9645def9259f606c
jony <jonyc88@gmail.com>
2017-04-05 12:43:13 +0530

    dashboard changed

--- src/main/java/com/hartron/investharyana/web/rest/DepartmentStatsResource.java
+++ src/main/java/com/hartron/investharyana/web/rest/DepartmentStatsResource.java
@@ -43,7 +43,7 @@ public class DepartmentStatsResource {
     public List<DepartmentStatsDTO> getAllDepartmentStats() {
         log.debug("REST request to get all DepartmentStats");
 
-        DepartmentStatsDTO departmentStatsDTO=new DepartmentStatsDTO();
+
 
         List<DepartmentStatsDTO> departmentStatsDTOList=new ArrayList<>();
 
@@ -65,16 +65,50 @@ public class DepartmentStatsResource {
 //            }
 //        }
 
+        DepartmentStatsDTO departmentStatsDTO=new DepartmentStatsDTO();
         departmentStatsDTO.setDepartmentName("HAFED");
         departmentStatsDTO.setTotalCaf("89");
         departmentStatsDTO.setTotalPendingCaf("65");
         departmentStatsDTO.setTotalStageOneCaf("28");
         departmentStatsDTO.setTotalStageTwoCaf("12");
         departmentStatsDTO.setTotalStageThreeCaf("17");
-
         departmentStatsDTOList.add(departmentStatsDTO);
 
+        DepartmentStatsDTO departmentStatsDTO1=new DepartmentStatsDTO();
+        departmentStatsDTO1.setDepartmentName("Higher Education");
+        departmentStatsDTO1.setTotalCaf("40");
+        departmentStatsDTO1.setTotalPendingCaf("16");
+        departmentStatsDTO1.setTotalStageOneCaf("34");
+        departmentStatsDTO1.setTotalStageTwoCaf("17");
+        departmentStatsDTO1.setTotalStageThreeCaf("60");
+        departmentStatsDTOList.add(departmentStatsDTO1);
 
+        DepartmentStatsDTO departmentStatsDTO2=new DepartmentStatsDTO();
+        departmentStatsDTO2.setDepartmentName("HSAMV");
+        departmentStatsDTO2.setTotalCaf("12");
+        departmentStatsDTO2.setTotalPendingCaf("7");
+        departmentStatsDTO2.setTotalStageOneCaf("3");
+        departmentStatsDTO2.setTotalStageTwoCaf("1");
+        departmentStatsDTO2.setTotalStageThreeCaf("3");
+        departmentStatsDTOList.add(departmentStatsDTO2);
+
+        DepartmentStatsDTO departmentStatsDTO3=new DepartmentStatsDTO();
+        departmentStatsDTO3.setDepartmentName("Food & Supplies");
+        departmentStatsDTO3.setTotalCaf("76");
+        departmentStatsDTO3.setTotalPendingCaf("65");
+        departmentStatsDTO3.setTotalStageOneCaf("28");
+        departmentStatsDTO3.setTotalStageTwoCaf("11");
+        departmentStatsDTO3.setTotalStageThreeCaf("19");
+        departmentStatsDTOList.add(departmentStatsDTO3);
+
+        DepartmentStatsDTO departmentStatsDTO4=new DepartmentStatsDTO();
+        departmentStatsDTO4.setDepartmentName("Food & Supplies");
+        departmentStatsDTO4.setTotalCaf("76");
+        departmentStatsDTO4.setTotalPendingCaf("65");
+        departmentStatsDTO4.setTotalStageOneCaf("28");
+        departmentStatsDTO4.setTotalStageTwoCaf("11");
+        departmentStatsDTO4.setTotalStageThreeCaf("19");
+        departmentStatsDTOList.add(departmentStatsDTO4);
 
         return departmentStatsDTOList;
     }
